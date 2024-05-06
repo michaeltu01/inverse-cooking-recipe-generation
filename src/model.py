gradient bug

src/model.py
@@ -8,34 +8,6 @@ import numpy as np
 # NOTE: Replaced torch check for cuda
 device = tf.device('/GPU:0' if tf.config.experimental.list_physical_devices('GPU') else '/CPU:0')
 
-# def label2onehot(labels, pad_value):
-
-#     # input labels to one hot vector
-#     inp_ = tf.expand_dims(labels, 2)
-#     # one_hot = torch.FloatTensor(labels.size(0), labels.size(1), pad_value + 1).zero_().to(device)
-#     one_hot = tf.zeros((labels.shape[0], labels.shape[1], pad_value + 1))
-#     one_hot.scatter_(2, inp_, 1)
-#     one_hot, _ = one_hot.max(dim=1)
-#     # remove pad position
-#     one_hot = one_hot[:, :-1]
-#     # eos position is always 0
-#     one_hot[:, 0] = 0
-
-#     return one_hot
-
-# def label2onehot(labels, pad_value, label_smoothing=None):
-#     one_hot = tf.one_hot(labels, depth=pad_value+1, dtype=tf.float32)
-#     one_hot = tf.transpose(one_hot, perm=[0, 2, 1])
-    
-#     if label_smoothing is not None:
-#         one_hot = (1 - label_smoothing) * one_hot + label_smoothing / (pad_value + 1)
-    
-#     one_hot = tf.reduce_max(one_hot, axis=-1)
-#     one_hot = tf.cast(one_hot, dtype=tf.int32)
-#     one_hot = tf.one_hot(one_hot, depth=2, dtype=tf.float32)
-    
-#     return one_hot
-
 def label2onehot(labels, pad_value):
     # input labels to one hot vector
     inp_ = tf.expand_dims(labels, axis=-1)
@@ -98,6 +70,19 @@ def get_model(args, ingr_vocab_size, instrs_vocab_size):
                                       normalize_inputs=True,
                                       last_ln=True,
                                       scale_embed_grad=False)
+    
+    decoder.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate, weight_decay=args.weight_decay),
+        loss='binary_crossentropy',
+        metrics=[softIoU]
+    )
+
+    ingr_decoder.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate, weight_decay=args.weight_decay),
+        loss='binary_crossentropy',
+        metrics=[softIoU]
+    )
+
     # recipe loss
     criterion = MaskedCrossEntropyCriterion(ignore_index=[instrs_vocab_size-1], reduce=False)
 
@@ -111,6 +96,14 @@ def get_model(args, ingr_vocab_size, instrs_vocab_size):
                                 pad_value=ingr_vocab_size-1,
                                 ingrs_only=args.ingrs_only, recipe_only=args.recipe_only,
                                 label_smoothing=args.label_smoothing_ingr)
+    
+    optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate, weight_decay=args.weight_decay)
+    
+    model.compile(
+        optimizer=optimizer,
+        loss='binary_crossentropy',
+        metrics=[softIoU]
+    )
 
     return model
 
@@ -142,9 +135,9 @@ class InverseCookingModel(tf.keras.Model):
         targets = captions[:, 1:]
         targets = tf.reshape(targets, [-1])
 
-        print("img_inputs in image encoder", img_inputs)
+        # print("img_inputs in image encoder", img_inputs)
         img_features = self.image_encoder(img_inputs, keep_cnn_gradients=keep_cnn_gradients)
-        print("image features shape out of encoder", img_features.shape)
+        # print("image features shape out of encoder", img_features.shape)
 
         losses = {}
         target_one_hot = label2onehot(target_ingrs, self.pad_value)
@@ -243,12 +236,13 @@ class InverseCookingModel(tf.keras.Model):
 
         target_ingr_mask = tf.expand_dims(tf.cast(target_ingr_mask, tf.float32), axis=1)
 
-        outputs, ids = self.recipe_decoder(target_ingr_feats, target_ingr_mask, captions, img_features)
+        outputs, ids = self.recipe_decoder(target_ingr_feats, target_ingr_mask, captions, img_features, training=training)
 
         outputs = outputs[:, :-1, :]
-        outputs = outputs.view(tf.shape(outputs)[0] * tf.shape(outputs)[1], -1)
+        outputs = tf.reshape(outputs, [tf.shape(outputs)
+        [0] * tf.shape(outputs)[1], -1])
 
-        loss = self.crit(outputs, targets)
+        loss = self.crit(outputs, targets) # MaskedCrossEntropyCriterion takes outputs, then targets
 
         losses['recipe_loss'] = loss
 

src/modules/encoder.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 from tensorflow import nn
 from keras import Sequential
 from keras.applications import ResNet101
-# vgg16, vgg19, inception_v3
 import random
 import numpy as np
 
@@ -20,16 +19,16 @@ class EncoderCNN(tf.keras.layers.Layer):
                                   tf.keras.layers.Dropout(dropout)])
 
     def call(self, images, keep_cnn_gradients=False):
-        print("images shape", images.shape)
+        # print("images shape", images.shape)
         if keep_cnn_gradients:
             raw_conv_feats = self.resnet(images)
         else:
             raw_conv_feats = tf.stop_gradient(self.resnet(images))
-        print("raw conv feats", raw_conv_feats.shape)
+        # print("raw conv feats", raw_conv_feats.shape)
         features = self.linear(raw_conv_feats)
-        print("features shape after convolution", features.shape)
+        # print("features shape after convolution", features.shape)
         features = tf.reshape(features, [tf.shape(features)[0], -1, tf.shape(features)[-1]])
-        print("returning this shape", features.shape)
+        # print("returning this shape", features.shape)
         return features
 
 
@@ -37,7 +36,7 @@ class EncoderLabels(tf.keras.layers.Layer):
     def __init__(self, embed_size, num_classes, dropout=0.5, embed_weights=None, scale_grad=False):
         super(EncoderLabels, self).__init__()
         # embeddinglayer = nn.Embedding(num_classes, embed_size, padding_idx=num_classes-1, scale_grad_by_freq=scale_grad)
-        # TODO: need to pad and somehow pass the last pytorch argument
+        # need to pad and somehow pass the last pytorch argument
         if embed_weights is not None:
             embeddinglayer = tf.keras.layers.Embedding(num_classes, embed_size, mask_zero=True, weights=embed_weights)
         else:

src/modules/multihead_attention.py
@@ -43,6 +43,8 @@ class MultiheadAttention(tf.keras.layers.Layer):
         # TODO: may have to specifiy that input size is embed_size
         self.out_proj = tf.keras.layers.Dense(embed_dim, use_bias=bias)
 
+        self.dropout_layer = tf.keras.layers.Dropout(rate=dropout)
+
         # self.reset_parameters()
 
     # def reset_parameters(self):
@@ -52,8 +54,9 @@ class MultiheadAttention(tf.keras.layers.Layer):
     #         nn.init.constant_(self.in_proj_bias, 0.)
     #         nn.init.constant_(self.out_proj.bias, 0.)
 
-    def call(self, query, key, value, key_padding_mask=None, training=False, 
-                mask_future_timesteps=False):
+    def call(self, query, key, value, mask_future_timesteps=False,
+                key_padding_mask=None, incremental_state=None,
+                need_weights=True, static_kv=False, training=False):
         """Input shape: Time x Batch x Channel
         Self-attention can be implemented by passing in the same arguments for
         query, key and value. Future timesteps can be masked with the
@@ -62,42 +65,172 @@ class MultiheadAttention(tf.keras.layers.Layer):
         batch x src_len, where padding elements are indicated by 1s.
         """
 
-        batch_size, seq_length, embed_dim = query.shape
+        qkv_same = id(query) == id(key) == id(value)
+        kv_same = id(key) == id(value)
+        
+        tgt_len, bsz, embed_dim = query.shape
         assert embed_dim == self.embed_dim
 
-        qkv = tf.linalg.matmul(query, self.in_proj_weight, transpose_b=True) + self.in_proj_bias
-        q, k, v = tf.split(qkv, 3, axis=-1)
+        if qkv_same:
+            q, k, v = self.in_proj_qkv(query)
+        elif kv_same:
+            q = self.in_proj_q(query)
+            if key is None:
+                assert value is None
+                k = v = tf.constant(0, dtype=q.dtype)
+            else:
+                k, v = self.in_proj_kv(key)
+        else:
+            q = self.in_proj_q(query)
+            k = self.in_proj_k(key)
+            v = self.in_proj_v(value)
         q *= self.scaling
 
-        q = tf.reshape(q, (batch_size, seq_length, self.num_heads, self.head_dim))
-        k = tf.reshape(k, (batch_size, seq_length, self.num_heads, self.head_dim))
-        v = tf.reshape(v, (batch_size, seq_length, self.num_heads, self.head_dim))
-        q = tf.transpose(q, [0, 2, 1, 3])
-        k = tf.transpose(k, [0, 2, 3, 1])
-        v = tf.transpose(v, [0, 2, 1, 3])
+        src_len = tf.shape(k)[0]
 
-        # attn_weights = tf.matmul(q, tf.transpose(k, [0, 2, 1]))
-        attn_weights = tf.matmul(q, k)
-
-        # only apply masking at training time (when incremental state is None)
-        if mask_future_timesteps:
-            mask_shape = [1, 1, seq_length, seq_length]
-            future_mask = tf.linalg.band_part(tf.ones(mask_shape), -1, 0)
-            attn_weights -= 1e9 * (1 - future_mask)
         if key_padding_mask is not None:
-            mask = tf.reshape(mask, [batch_size, 1, 1, seq_length])
-            attn_weights += (mask * -1e9)
-        # CHANGED: F.softmax --> tf.nn
-        # normalize attention scores
-        attn_weights = tf.nn.softmax(attn_weights, axis=-1)
-        attn_weights = tf.nn.dropout(attn_weights, rate=self.dropout if training else 0.0)
+            assert key_padding_mask.size(0) == bsz
+            assert key_padding_mask.size(1) == src_len
 
-        #CHANGED: torch.bmm --> tf.matmul
-        # apply attention to get weight average
-        attn_output = tf.matmul(attn_weights, v)
-        attn_output = tf.transpose(attn_output, [0, 2, 1, 3])
-        attn_output = tf.reshape(attn_output, [batch_size, seq_length, self.embed_dim])
+        q = tf.transpose(q, [1, 0, 2])
+        k = tf.transpose(k, [1, 0, 2])
+        v = tf.transpose(v, [1, 0, 2])
 
-        output = self.out_proj(attn_output)
+        q = tf.reshape(q, [bsz * self.num_heads, -1, self.head_dim])
+        k = tf.reshape(k, [bsz * self.num_heads, -1, self.head_dim])
+        v = tf.reshape(v, [bsz * self.num_heads, -1, self.head_dim])
 
-        return output, attn_weights
+        attn_weights = tf.linalg.matmul(q, k, transpose_b=True)
+
+        if mask_future_timesteps and incremental_state is None:
+            # assert tf.shape(query) == tf.shape(key), \
+            assert all([tf.shape(query)[i] == tf.shape(key)[i] for i in range(len(tf.shape(query)))]), \
+                'mask_future_timesteps only applies to self-attention'
+            attn_weights += tf.expand_dims(self.buffered_mask(attn_weights), axis=0)
+
+        if key_padding_mask is not None:
+            key_padding_mask = tf.cast(key_padding_mask, tf.float32)
+            mask = key_padding_mask[:, tf.newaxis, tf.newaxis, :]
+            adder = (1.0 - mask) * -1e9
+            k += adder
+            v += adder
+
+        attn_weights = tf.nn.softmax(attn_weights, axis = -1)
+        attn_weights = self.dropout_layer(attn_weights, training=training)
+
+        attn = tf.linalg.matmul(attn_weights, v)
+        attn = tf.reshape(attn, [bsz, -1, self.embed_dim])
+        attn = tf.transpose(attn, [1, 0, 2])
+        attn = self.out_proj(attn)
+
+        attn_weights = tf.reshape(attn_weights, [bsz, self.num_heads, tf.shape(query)[0], -1])
+        attn_weights = tf.reduce_mean(attn_weights, axis=1)
+
+        return attn, attn_weights
+
+        # # --------------------
+        # qkv = tf.linalg.matmul(query, self.in_proj_weight, transpose_b=True) + self.in_proj_bias
+        # q, k, v = tf.split(qkv, 3, axis=-1)
+        # q *= self.scaling
+
+        # q = tf.reshape(q, (batch_size, seq_length, self.num_heads, self.head_dim))
+        # k = tf.reshape(k, (batch_size, seq_length, self.num_heads, self.head_dim))
+        # v = tf.reshape(v, (batch_size, seq_length, self.num_heads, self.head_dim))
+        # q = tf.transpose(q, [0, 2, 1, 3])
+        # k = tf.transpose(k, [0, 2, 3, 1])
+        # v = tf.transpose(v, [0, 2, 1, 3])
+
+        # # attn_weights = tf.matmul(q, tf.transpose(k, [0, 2, 1]))
+        # attn_weights = tf.matmul(q, k)
+
+        # # only apply masking at training time (when incremental state is None)
+        # if mask_future_timesteps:
+        #     mask_shape = [1, 1, seq_length, seq_length]
+        #     future_mask = tf.linalg.band_part(tf.ones(mask_shape), -1, 0)
+        #     attn_weights -= 1e9 * (1 - future_mask)
+        # if key_padding_mask is not None:
+        #     mask = tf.reshape(mask, [batch_size, 1, 1, seq_length])
+        #     attn_weights += (mask * -1e9)
+        # # CHANGED: F.softmax --> tf.nn
+        # # normalize attention scores
+        # attn_weights = tf.nn.softmax(attn_weights, axis=-1)
+        # attn_weights = self.dropout_layer(self.dropout, training=)
+
+        # #CHANGED: torch.bmm --> tf.matmul
+        # # apply attention to get weight average
+        # attn_output = tf.matmul(attn_weights, v)
+        # attn_output = tf.transpose(attn_output, [0, 2, 1, 3])
+        # attn_output = tf.reshape(attn_output, [batch_size, seq_length, self.embed_dim])
+
+        # output = self.out_proj(attn_output)
+
+        # return output, attn_weights
+    
+    def in_proj_qkv(self, query):
+        return tf.split(self._in_proj(query), num_or_size_splits=3, axis=-1)
+
+    def in_proj_kv(self, key):
+        # return self._in_proj(key, start=self.embed_dim).chunk(2, dim=-1)
+        return tf.split(self._in_proj(key, start=self.embed_dim), num_or_size_splits=2, axis=-1)
+
+    def in_proj_q(self, query):
+        return self._in_proj(query, end=self.embed_dim)
+
+    def in_proj_k(self, key):
+        return self._in_proj(key, start=self.embed_dim, end=2*self.embed_dim)
+
+    def in_proj_v(self, value):
+        return self._in_proj(value, start=2*self.embed_dim)
+
+    def _in_proj(self, input, start=None, end=None):
+        weight = self.in_proj_weight
+        bias = self.in_proj_bias
+        if end is not None:
+            weight = weight[:end, :]
+            if bias is not None:
+                bias = bias[:end]
+        if start is not None:
+            weight = weight[start:, :]
+            if bias is not None:
+                bias = bias[start:]
+        # return F.linear(input, weight, bias)
+        # input = tf.transpose(input, perm= [1, 2, 0])
+        result = tf.linalg.matmul(input, weight, transpose_b=True)
+        if bias is not None:
+            result += bias
+        return result
+
+    def fill_with_neg_inf(self, t):
+        """FP16-compatible function that fills a tensor with -inf."""
+        output = tf.cast(tf.fill(tf.shape(t), tf.constant(float('-inf'))), dtype=t.dtype) 
+        return output
+
+    def buffered_mask(self, tensor):
+        dim = tf.shape(tensor)[-1]
+        if self._mask is None:
+            self._mask = tf.linalg.band_part(self.fill_with_neg_inf(tf.zeros_like(tensor)), 0, -1)
+        if tf.shape(self._mask)[0] < dim:
+            self._mask = tf.linalg.band_part(self.fill_with_neg_inf(tf.resize(self._mask, (dim, dim))), 0, -1)
+        return self._mask[:dim, :dim]
+
+    # def reorder_incremental_state(self, incremental_state, new_order):
+    #     """Reorder buffered internal state (for incremental generation)."""
+    #     input_buffer = self._get_input_buffer(incremental_state)
+    #     if input_buffer is not None:
+    #         for k in input_buffer.keys():
+    #             input_buffer[k] = input_buffer[k].index_select(1, new_order)
+    #         self._set_input_buffer(incremental_state, input_buffer)
+
+    # def _get_input_buffer(self, incremental_state):
+    #     return get_incremental_state(
+    #         self,
+    #         incremental_state,
+    #         'attn_state',
+    #     ) or {}
+
+    # def _set_input_buffer(self, incremental_state, buffer):
+    #     set_incremental_state(
+    #         self,
+    #         incremental_state,
+    #         'attn_state',
+    #         buffer,

src/modules/transformer_decoder.py
@@ -165,11 +165,13 @@ class TransformerDecoderLayer(tf.keras.layers.Layer):
 
         # attention
         if ingr_features is None:
+            img_features = tf.transpose(img_features, perm= [1, 2, 0])
 
             x, _ = self.cond_att(query=x,
                                     key=img_features,
                                     value=img_features,
-                                    key_padding_mask=None
+                                    key_padding_mask=None, 
+                                    training = training
                                     )
         elif img_features is None:
             x, _ = self.cond_att(query=x,
@@ -178,6 +180,7 @@ class TransformerDecoderLayer(tf.keras.layers.Layer):
                                     key_padding_mask=ingr_mask,
                                     incremental_state=incremental_state,
                                     static_kv=True,
+                                    training= training
                                     )
 
 
@@ -228,8 +231,8 @@ class DecoderTransformer(tf.keras.Model):
         super(DecoderTransformer, self).__init__()
         self.dropout = dropout
         self.seq_length = seq_length * num_instrs
-        self.embed_tokens = tf.keras.layers.Embedding(vocab_size, embed_size, embeddings_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=embed_size**-0.5))
-        self.final_ln = tf.keras.layers.LayerNormalization()
+        self.embed_tokens = tf.keras.layers.Embedding(vocab_size, embed_size, embeddings_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=embed_size**-0.5), name="decoder_transformer_embed_tokens")
+        self.final_ln = tf.keras.layers.LayerNormalization(name='final_layer_norm_decoder_transformer')
         if pos_embeddings:
             self.embed_positions = PositionalEmbedding(1024, embed_size, padding_idx=0, left_pad=False, learned=learned)
         else:

src/sample.py
@@ -94,7 +94,7 @@ def main(args):
     # model.load_state_dict(torch.load(model_path, map_location=map_loc))
     model.load_weights(model_path)
 
-    model.eval()
+    # model.eval()
     results_dict = {'recipes': {}, 'ingrs': {}, 'ingr_iou': {}}
     captions = {}
     iou = []
@@ -103,7 +103,7 @@ def main(args):
     n_rep, th = 0, 0.3
 
     for i, (img_inputs, true_caps_batch, ingr_gt, imgid, impath) in tqdm(enumerate(data_loader)):
-        true_caps_shift = true_caps_batch.clone()[:, 1:].contiguous()
+        true_caps_shift = tf.identity(true_caps_batch[:, 1:])
         true_ingrs = ingr_gt if args.use_true_ingrs else None
         for gens in range(args.numgens):
             if args.get_perplexity:
@@ -139,7 +139,7 @@ def main(args):
 
                 if not args.ingrs_only:
                     sampled_ids_batch = outputs['recipe_ids']
-                    sampled_ids_batch = sampled_ids_batch.cpu().detach().numpy()
+                    sampled_ids_batch = tf.stop_gradient(sampled_ids_batch).numpy()
 
                     for j, sampled_ids in enumerate(sampled_ids_batch):
                         score = compute_score(sampled_ids)

src/train.py
@@ -75,6 +75,7 @@ def main(args):
         logger = Visualizer(tb_logs, name='visual_results')
 
     # check if we want to resume from last checkpoint of current model
+    # NOTE: Lowkey unnecessary
     if args.resume:
         args = pickle.load(open(os.path.join(checkpoints_dir, 'args.pkl'), 'rb'))
         args.resume = True
@@ -125,13 +126,14 @@ def main(args):
 
     # Build the model
     model = get_model(args, ingr_vocab_size, instrs_vocab_size)
+    keep_cnn_gradients = False #NEW
     decay_factor = 1.0
     
-    dummy_inputs, dummy_captions, dummy_ingr_gt, _, _ = next(iter(data_loaders['train']))
-    # print("inputs", dummy_inputs)
-    # print("captions", dummy_captions)
-    # print("ingrs", dummy_ingr_gt)
-    dummy_outputs = model(dummy_inputs, dummy_captions, dummy_ingr_gt, training=False)
+    # dummy_inputs, dummy_captions, dummy_ingr_gt, _, _ = next(iter(data_loaders['train']))
+    # print("inputs", tf.shape(dummy_inputs))
+    # print("captions", tf.shape(dummy_captions))
+    # print("ingrs", tf.shape(dummy_ingr_gt))
+    # dummy_outputs = model(dummy_inputs, dummy_captions, dummy_ingr_gt, training=False)
 
     # add model parameters
     if args.ingrs_only:
@@ -141,8 +143,8 @@ def main(args):
     else:
         params = model.recipe_decoder.trainable_variables + model.ingredient_decoder.trainable_variables \
                 + model.ingredient_encoder.trainable_variables
-    print(params)
     # only train the linear layer in the encoder if we are not transfering from another model
+    # NOTE: Lowkey unnecessary
     if args.transfer_from == '':
         params += model.image_encoder.linear.trainable_variables
     params_cnn = model.image_encoder.resnet.trainable_variables
@@ -151,14 +153,18 @@ def main(args):
     print("CNN params:", sum(tf.size(p).numpy() for p in model.image_encoder.trainable_variables))
     print("Decoder params:", sum(tf.size(p).numpy() for p in params))
 
-    optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate, weight_decay=args.weight_decay)
+    # optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate, weight_decay=args.weight_decay)
 
+    # NOTE: Removed fine-tuning procedure
+
+    # NOTE: Lowkey unnecessary
     if args.resume:
         model_path = os.path.join(args.save_dir, args.project_name, args.model_name, 'checkpoints', 'modelbest.ckpt')
         optim_path = os.path.join(args.save_dir, args.project_name, args.model_name, 'checkpoints', 'optimbest.ckpt')
         model.load_weights(model_path)
-        optimizer.load_weights(optim_path)
+        model.optimizer.load_weights(optim_path)
 
+    # NOTE: Lowkey unnecessary
     if args.transfer_from != '':
         model_path = os.path.join(args.save_dir, args.project_name, args.transfer_from, 'checkpoints', 'modelbest.ckpt')
         pretrained_model = get_model(args, ingr_vocab_size, instrs_vocab_size)
@@ -177,10 +183,7 @@ def main(args):
     es_best = 10000 if args.es_metric == 'loss' else 0
     # Train the model
     start = args.current_epoch
-
-
     for epoch in range(start, args.num_epochs):
-
         # save current epoch for resuming
         if args.tensorboard:
             logger.reset()
@@ -192,8 +195,9 @@ def main(args):
             decay_factor = args.lr_decay_rate ** frac
             new_lr = args.learning_rate*decay_factor
             print ('Epoch %d. lr: %.5f'%(epoch, new_lr))
-            optimizer.learning_rate.assign(new_lr)
+            model.optimizer.learning_rate.assign(new_lr)
 
+        # NOTE: Please don't runnn
         if args.finetune_after != -1 and args.finetune_after < epoch \
                 and not keep_cnn_gradients and params_cnn is not None:
 
@@ -203,12 +207,21 @@ def main(args):
             #                               {'params': params_cnn,
             #                                'lr': decay_factor*args.learning_rate*args.scale_learning_rate_cnn}],
             #                              lr=decay_factor*args.learning_rate)
-            optimizer.learning_rate.assign(decay_factor * args.learning_rate)
+            model.optimizer.learning_rate.assign(decay_factor * args.learning_rate)
             keep_cnn_gradients = True
 
         for split in ['train', 'val']:
             total_step = len(data_loaders[split])
             loader = iter(data_loaders[split])
+
+            total_loss_dict = {'recipe_loss': [], 'ingr_loss': [],
+                                'eos_loss': [], 'loss': [],
+                                'iou': [], 'perplexity': [], 'iou_sample': [],
+                                'f1': [],
+                                'card_penalty': []}
+            error_types = {'tp_i': 0, 'fp_i': 0, 'fn_i': 0, 'tn_i': 0,
+                        'tp_all': 0, 'fp_all': 0, 'fn_all': 0}
+
             start = time.time()
             for i in range(total_step):
 
@@ -218,52 +231,39 @@ def main(args):
                 captions = tf.convert_to_tensor(captions.numpy())
                 ingr_gt = tf.convert_to_tensor(ingr_gt.numpy())
                 true_caps_batch = captions[:, 1:]
-
                 loss_dict = {}
 
-
-                if split == 'train':
-                    model(img_inputs, captions, ingr_gt, sample=False, training = True)
-                else:
-                    model.eval()
-                total_step = len(data_loaders[split])
-                loader = iter(data_loaders[split])
-
-                total_loss_dict = {'recipe_loss': [], 'ingr_loss': [],
-                                'eos_loss': [], 'loss': [],
-                                'iou': [], 'perplexity': [], 'iou_sample': [],
-                                'f1': [],
-                                'card_penalty': []}
-
-                error_types = {'tp_i': 0, 'fp_i': 0, 'fn_i': 0, 'tn_i': 0,
-                            'tp_all': 0, 'fp_all': 0, 'fn_all': 0}
-                
-
+                # if split == 'train':
+                #     model(img_inputs, captions, ingr_gt, sample=False, training = True)
+                # else:
+                #     model.eval()
+                # total_step = len(data_loaders[split])
+                # loader = iter(data_loaders[split])
                 if split == 'val':
                     losses = model(img_inputs, captions, ingr_gt, training=False)
 
                     if not args.recipe_only:
+                        outputs = model(img_inputs, captions, ingr_gt, sample=True)
                         ingr_ids_greedy = outputs['ingr_ids']
 
                         mask = mask_from_eos(ingr_ids_greedy, eos_value=0, mult_before=False)
                         ingr_ids_greedy = tf.where(mask == 0, ingr_vocab_size - 1, ingr_ids_greedy)
                         pred_one_hot = label2onehot(ingr_ids_greedy, ingr_vocab_size - 1)
                         target_one_hot = label2onehot(ingr_gt, ingr_vocab_size - 1)
-
                         iou_sample = softIoU(pred_one_hot, target_one_hot)
-                        iou_sample = tf.reduce_sum(iou_sample) / (tf.size(iou_sample) + 1e-6) 
+                        # iou_sample = tf.reduce_sum(iou_sample) / (tf.shape(iou_sample)[0] + 1e-6) 
+                        iou_sample = tf.reduce_sum(iou_sample) / (tf.math.count_nonzero(iou_sample) + 1e-6)
                         loss_dict['iou_sample'] = iou_sample.numpy()
 
                         update_error_types(error_types, pred_one_hot, target_one_hot)
 
                         del outputs, pred_one_hot, target_one_hot, iou_sample
-
                 else:
                     with tf.GradientTape() as tape:
+                        tape.watch(model.trainable_variables)
                         losses = model(img_inputs, captions, ingr_gt, training=True)
-                    gradients = tape.gradient(losses, model.trainable_variables)
-                    print(gradients, "gradient")
-                    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+                    # gradients = tape.gradient(losses, model.trainable_variables)
+                    # optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
                 if not args.ingrs_only:
                     recipe_loss = losses['recipe_loss']
@@ -272,14 +272,13 @@ def main(args):
                     non_pad_mask = tf.cast(tf.not_equal(true_caps_batch, instrs_vocab_size - 1), tf.float32)
 
                     recipe_loss_masked = tf.reduce_sum(recipe_loss * non_pad_mask, axis=-1) / tf.reduce_sum(non_pad_mask, axis=-1)
-
                     perplexity = tf.exp(recipe_loss_masked)
 
                     recipe_loss = tf.reduce_mean(recipe_loss_masked)
                     perplexity = tf.reduce_mean(perplexity)
 
-                    loss_dict['recipe_loss'] = recipe_loss
-                    loss_dict['perplexity'] = perplexity
+                    loss_dict['recipe_loss'] = recipe_loss.numpy()
+                    loss_dict['perplexity'] = perplexity.numpy()
                 else:
                     recipe_loss = 0
 
@@ -316,11 +315,10 @@ def main(args):
                     total_loss_dict[key].append(loss_dict[key])
 
                 if split == 'train':
-                    with tf.GradientTape() as tape:
-                        total_loss = tf.convert_to_tensor(sum(loss_dict.values()))
-                    gradients = tape.gradient(total_loss, model.trainable_variables)
-                    zipped_grads = zip(gradients, model.trainable_variables)
-                    optimizer.apply_gradients(zipped_grads)
+                    # with tf.GradientTape() as tape:
+                    #     total_loss = tf.convert_to_tensor(sum(loss_dict.values()))
+                    gradients = tape.gradient(total_loss, params)
+                    model.optimizer.apply_gradients(zip(gradients, params))
 
                 if args.log_step != -1 and i % args.log_step == 0:
                     elapsed_time = time.time() - start
@@ -335,12 +333,13 @@ def main(args):
                     print(strtoprint)
 
                     if args.tensorboard:
-                        with logger.as_default():
-                            for k, v in total_loss_dict.items():
-                                if len(v) > 0:
-                                    tf.summary.scalar(k, np.mean(v[-args.log_step:]), step=total_step * epoch + i)
-                            tf.summary.flush()
-
+                        # with logger.as_default():
+                        #     for k, v in total_loss_dict.items():
+                        #         if len(v) > 0:
+                        #             tf.summary.scalar(k, np.mean(v[-args.log_step:]), step=total_step * epoch + i)
+                        #     tf.summary.flush()
+                        logger.scalar_summary(mode=split+'_iter', epoch=total_step*epoch+i,
+                                              **{k: np.mean(v[-args.log_step:]) for k, v in total_loss_dict.items() if v})
                     start = time.time()
                 del loss, losses, captions, img_inputs
 
@@ -361,10 +360,10 @@ def main(args):
         es_value = np.mean(total_loss_dict[args.es_metric])
 
         # save current model as well
-        save_model(model, optimizer, checkpoints_dir, suff='')
+        save_model(model, model.optimizer, checkpoints_dir, suff='')
         if (args.es_metric == 'loss' and es_value < es_best) or (args.es_metric == 'iou_sample' and es_value > es_best):
             es_best = es_value
-            save_model(model, optimizer, checkpoints_dir, suff='best')
+            save_model(model, model.optimizer, checkpoints_dir, suff='best')
             pickle.dump(args, open(os.path.join(checkpoints_dir, 'args.pkl'), 'wb'))
             curr_pat = 0
             print('Saved checkpoint.')