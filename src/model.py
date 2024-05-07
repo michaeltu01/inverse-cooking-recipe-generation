gradient bug

src/model.py
@@ -54,7 +54,7 @@ def get_model(args, ingr_vocab_size, instrs_vocab_size):
     decoder = DecoderTransformer(args.embed_size, instrs_vocab_size,
                                  dropout=args.dropout_decoder_r, seq_length=args.maxseqlen,
                                  num_instrs=args.maxnuminstrs,
-                                 attention_nheads=args.n_att, num_layers=args.transf_layers,
+                                 attention_nheads=1, num_layers=1,
                                  normalize_before=True,
                                  normalize_inputs=False,
                                  last_ln=False,
@@ -62,9 +62,9 @@ def get_model(args, ingr_vocab_size, instrs_vocab_size):
 
     ingr_decoder = DecoderTransformer(args.embed_size, ingr_vocab_size, dropout=args.dropout_decoder_i,
                                       seq_length=args.maxnumlabels,
-                                      num_instrs=1, attention_nheads=args.n_att_ingrs,
+                                      num_instrs=1, attention_nheads=1,
                                       pos_embeddings=False,
-                                      num_layers=args.transf_layers_ingrs,
+                                      num_layers=1,
                                       learned=False,
                                       normalize_before=True,
                                       normalize_inputs=True,
@@ -128,10 +128,10 @@ class InverseCookingModel(tf.keras.Model):
         self.label_smoothing = label_smoothing
 
     # Changed to a more familiar 'call' function, instead of 'forward'
-    def call(self, img_inputs, captions, target_ingrs, sample=False, keep_cnn_gradients=False, training = False):
+    def call(self, img_inputs, captions, target_ingrs, sample=False, keep_cnn_gradients=False, training = True):
+        print("Training flag in call:", training)
         if sample:
-            return self.sample(img_inputs, greedy=True)
-
+            return self.sample(img_inputs, greedy=True, training = training)
         targets = captions[:, 1:]
         targets = tf.reshape(targets, [-1])
 
@@ -163,7 +163,7 @@ class InverseCookingModel(tf.keras.Model):
             # autoregressive mode for ingredient decoder
             ingr_ids, ingr_logits = self.ingredient_decoder.sample(None, None, greedy=True,
                                                                    temperature=1.0, img_features=img_features,
-                                                                   first_token_value=0, replacement=False)
+                                                                   first_token_value=0, replacement=False, training = training)
 
             # NOTE: torch.nn.functional.softmax -> tf.nn.softmax
             ingr_logits = tf.nn.softmax(ingr_logits, axis=-1)
@@ -199,8 +199,6 @@ class InverseCookingModel(tf.keras.Model):
             # NOTE: Replaced torch.abs -> tf.math.abs
             losses['card_penalty'] = tf.math.abs(tf.reduce_sum(ingr_probs*target_one_hot, axis=1)) - tf.reduce_sum(target_one_hot, axis=1) + \
                                      tf.math.abs(tf.reduce_sum(ingr_probs*(1-target_one_hot), axis=1))
-            print(ingr_probs, "ingr_probs")
-            print(target_one_hot, "target OH")
 
             eos_loss = self.crit_eos(eos, tf.cast(target_eos, tf.float32))
 
@@ -250,7 +248,7 @@ class InverseCookingModel(tf.keras.Model):
 
         return losses
     
-    def sample(self, img_inputs, greedy=True, temperature=1.0, beam=-1, true_ingrs=None):
+    def sample(self, img_inputs, greedy=True, temperature=1.0, beam=-1, true_ingrs=None, training = False):
 
         outputs = dict()
 
@@ -260,7 +258,7 @@ class InverseCookingModel(tf.keras.Model):
             ingr_ids, ingr_probs = self.ingredient_decoder.sample(None, None, greedy=True, temperature=temperature,
                                                                   beam=-1,
                                                                   img_features=img_features, first_token_value=0,
-                                                                  replacement=False)
+                                                                  replacement=False, training=training)
 
             # mask ingredients after finding eos
             sample_mask = mask_from_eos(ingr_ids, eos_value=0, mult_before=False)

src/modules/multihead_attention.py
@@ -64,17 +64,22 @@ class MultiheadAttention(tf.keras.layers.Layer):
         the key by passing a binary ByteTensor (`key_padding_mask`) with shape:
         batch x src_len, where padding elements are indicated by 1s.
         """
-
+        # tf.debugging.check_numerics(query, "query nan found")
         qkv_same = id(query) == id(key) == id(value)
         kv_same = id(key) == id(value)
         
         tgt_len, bsz, embed_dim = query.shape
         assert embed_dim == self.embed_dim
-
+        
         if qkv_same:
             q, k, v = self.in_proj_qkv(query)
+
         elif kv_same:
+            
             q = self.in_proj_q(query)
+ 
+            # print(k, "k")
+            # print(v, "v")
             if key is None:
                 assert value is None
                 k = v = tf.constant(0, dtype=q.dtype)
@@ -101,6 +106,7 @@ class MultiheadAttention(tf.keras.layers.Layer):
         v = tf.reshape(v, [bsz * self.num_heads, -1, self.head_dim])
 
         attn_weights = tf.linalg.matmul(q, k, transpose_b=True)
+        # print(attn_weights, "mha attn weights output")
 
         if mask_future_timesteps and incremental_state is None:
             # assert tf.shape(query) == tf.shape(key), \
@@ -125,6 +131,9 @@ class MultiheadAttention(tf.keras.layers.Layer):
 
         attn_weights = tf.reshape(attn_weights, [bsz, self.num_heads, tf.shape(query)[0], -1])
         attn_weights = tf.reduce_mean(attn_weights, axis=1)
+
+        # print(attn, "mha attn output")
+        
 
         return attn, attn_weights
 
@@ -202,7 +211,7 @@ class MultiheadAttention(tf.keras.layers.Layer):
 
     def fill_with_neg_inf(self, t):
         """FP16-compatible function that fills a tensor with -inf."""
-        output = tf.cast(tf.fill(tf.shape(t), tf.constant(float('-inf'))), dtype=t.dtype) 
+        output = tf.fill(tf.shape(t), tf.constant(float('-inf')))
         return output
 
     def buffered_mask(self, tensor):
@@ -210,7 +219,9 @@ class MultiheadAttention(tf.keras.layers.Layer):
         if self._mask is None:
             self._mask = tf.linalg.band_part(self.fill_with_neg_inf(tf.zeros_like(tensor)), 0, -1)
         if tf.shape(self._mask)[0] < dim:
-            self._mask = tf.linalg.band_part(self.fill_with_neg_inf(tf.resize(self._mask, (dim, dim))), 0, -1)
+            # self._mask = tf.linalg.band_part(self.fill_with_neg_inf(tf.reshape(self._mask, [dim, dim])), 0, -1)
+            new_mask = tf.zeros((dim, dim), dtype=tensor.dtype)
+            self._mask = tf.linalg.band_part(self.fill_with_neg_inf(new_mask), 0, -1) - tf.linalg.band_part(new_mask, 0, 0)
         return self._mask[:dim, :dim]
 
     # def reorder_incremental_state(self, incremental_state, new_order):

src/modules/transformer_decoder.py
@@ -131,7 +131,7 @@ class TransformerDecoderLayer(tf.keras.layers.Layer):
             dropout=dropout,
         )
 
-        self.cond_att = MultiheadAttention(
+        self.cond_attn = MultiheadAttention(
             self.embed_dim, n_att,
             dropout=dropout,
         )
@@ -145,36 +145,43 @@ class TransformerDecoderLayer(tf.keras.layers.Layer):
             self.last_ln = tf.keras.layers.LayerNormalization(epsilon=1e-5)
 
     def call(self, x, ingr_features, ingr_mask, incremental_state, img_features, training=False):
-
         # self attention
+        
         residual = x
         x = self.maybe_layer_norm(0, x, before=True)
+
         x, _ = self.self_attn(
             query=x,
             key=x,
             value=x,
-            mask_future_timesteps=True
+            mask_future_timesteps=True,
+            training = training
         )
+        
+
         dropout_layer = tf.keras.layers.Dropout(self.dropout)
         x = dropout_layer(x, training=training)
         x = residual + x
         x = self.maybe_layer_norm(0, x, after=True)
 
         residual = x
+        
         x = self.maybe_layer_norm(1, x, before=True)
-
         # attention
         if ingr_features is None:
             img_features = tf.transpose(img_features, perm= [1, 2, 0])
+            # img features here is populated with numbers
 
-            x, _ = self.cond_att(query=x,
+            x, _ = self.cond_attn(query=x,
                                     key=img_features,
                                     value=img_features,
                                     key_padding_mask=None, 
                                     training = training
                                     )
+            # some rows here have been replaced with nan 
+
         elif img_features is None:
-            x, _ = self.cond_att(query=x,
+            x, _ = self.cond_attn(query=x,
                                     key=ingr_features,
                                     value=ingr_features,
                                     key_padding_mask=ingr_mask,
@@ -226,9 +233,10 @@ class DecoderTransformer(tf.keras.Model):
     """Transformer decoder."""
 
     def __init__(self, embed_size, vocab_size, dropout=0.5, seq_length=20, num_instrs=15,
-                 attention_nheads=16, pos_embeddings=True, num_layers=8, learned=True, normalize_before=True,
+                 attention_nheads=16, pos_embeddings=True, num_layers=16, learned=True, normalize_before=True,
                  normalize_inputs=False, last_ln=False, scale_embed_grad=False):
         super(DecoderTransformer, self).__init__()
+        print(num_layers, "num_layers")
         self.dropout = dropout
         self.seq_length = seq_length * num_instrs
         self.embed_tokens = tf.keras.layers.Embedding(vocab_size, embed_size, embeddings_initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=embed_size**-0.5), name="decoder_transformer_embed_tokens")
@@ -246,8 +254,6 @@ class DecoderTransformer(tf.keras.Model):
         self.linear = tf.keras.layers.Dense(vocab_size-1)
 
     def call(self, ingr_features, ingr_mask, captions, img_features, incremental_state=None, training = False):
-        print(ingr_features, "ingr feat")
-        print(ingr_mask, "ingr mask")
         if ingr_features is not None:
             ingr_features = tf.transpose(ingr_features, perm=[0, 2, 1])
             ingr_features = tf.transpose(ingr_features, perm=[1, 0, 2])
@@ -283,25 +289,27 @@ class DecoderTransformer(tf.keras.Model):
         if self.normalize_inputs:
             x = self.layer_norms_in[2](x)
 
+
         x = tf.keras.layers.Dropout(self.dropout)(x, training=training)
 
         # B x T x C -> T x B x C
         # x = tf.transpose(x, perm=[1, 0] + list(range(2, tf.rank(x))))
-        x = tf.transpose(x, perm=[1, 0, 2])
-        print(x, "x")
+        # x = tf.transpose(x, perm=[1, 0, 2])
         for p, layer in enumerate(self.td_layers):
+            print (layer)
             x  = layer(
                 x,
                 ingr_features=ingr_features,
                 ingr_mask=ingr_mask,
                 incremental_state=incremental_state,
-                img_features=img_features
+                img_features=img_features,
+                training = training
             )
-        
         
         # T x B x C -> B x T x C
         # x = tf.transpose(x, perm=[1, 0] + list(range(2, tf.rank(x))))
-        x = tf.transpose(x, perm=[1, 0, 2])
+        #TODO:
+        # x = tf.transpose(x, perm=[1, 0, 2])
         x = self.linear(x)
         predicted = tf.argmax(x, axis=-1)
 
@@ -309,7 +317,7 @@ class DecoderTransformer(tf.keras.Model):
 
     def sample(self, ingr_features, ingr_mask, greedy=True, temperature=1.0, beam=-1,
                img_features=None, first_token_value=0,
-               replacement=True, last_token_value=0):
+               replacement=True, last_token_value=0, training = False):
 
         incremental_state = {}
 
@@ -319,7 +327,7 @@ class DecoderTransformer(tf.keras.Model):
         if beam != -1:
             if fs == 1:
                 return self.sample_beam(ingr_features, ingr_mask, beam, img_features, first_token_value,
-                                        replacement, last_token_value)
+                                        replacement, last_token_value, training = training)
             else:
                 print ("Beam Search can only be used with batch size of 1. Running greedy or temperature sampling...")
 
@@ -335,7 +343,7 @@ class DecoderTransformer(tf.keras.Model):
             caption_ids = [tf.cast(id, tf.int32) for id in sampled_ids]
             captions = tf.stack(caption_ids, axis=1)
 
-            outputs, _ = self.call(ingr_features=ingr_features, ingr_mask=ingr_mask, captions=captions, img_features=img_features, incremental_state=incremental_state)
+            outputs, _ = self.call(ingr_features=ingr_features, ingr_mask=ingr_mask, captions=captions, img_features=img_features, incremental_state=incremental_state, training=training)
             outputs = tf.squeeze(outputs, axis=1)
             if not replacement:
                 # predicted mask
@@ -384,7 +392,7 @@ class DecoderTransformer(tf.keras.Model):
         return sampled_ids, logits
 
     def sample_beam(self, ingr_features, ingr_mask, beam=3, img_features=None, first_token_value=0,
-                   replacement=True, last_token_value=0):
+                   replacement=True, last_token_value=0, training = False):
         k = beam
         alpha = 0.0
         # create dummy previous word
@@ -400,7 +408,7 @@ class DecoderTransformer(tf.keras.Model):
             all_candidates = []
             for rem in range(len(sequences)):
                 incremental = sequences[rem][2]
-                outputs, _ = self.call(ingr_features, ingr_mask, tf.stack(sequences[rem][0], axis=1), img_features, incremental)
+                outputs, _ = self.call(ingr_features, ingr_mask, tf.stack(sequences[rem][0], axis=1), img_features, incremental, training=training)
                 outputs = tf.squeeze(outputs, axis=1)
                 if not replacement:
                     # predicted mask

src/train.py
@@ -133,7 +133,7 @@ def main(args):
     print("inputs", tf.shape(dummy_inputs))
     print("captions", tf.shape(dummy_captions))
     print("ingrs", tf.shape(dummy_ingr_gt))
-    dummy_outputs = model(dummy_inputs, dummy_captions, dummy_ingr_gt, training=False)
+    dummy_outputs = model(dummy_inputs, dummy_captions, dummy_ingr_gt, training=True)
 
     # add model parameters
     if args.ingrs_only:
@@ -262,7 +262,6 @@ def main(args):
                     with tf.GradientTape() as tape:
                         tape.watch(model.trainable_variables)
                         losses = model(img_inputs, captions, ingr_gt, training=True)
-                        print(losses, "losses")
                     # gradients = tape.gradient(losses, model.trainable_variables)
                     # optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 